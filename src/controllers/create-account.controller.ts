2862d54c041dfb6f7b6a97face0760a5bf6a348d
PeZunino <pedrozsantos@hotmail.com>
2025-03-07 15:44:53 -0300

    feat(account): account controller and POST route

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,43 @@
+import { Body, ConflictException, Controller, Post, UsePipes } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController{
+
+  constructor(private prisma:PrismaService){}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body:CreateAccountBodySchema){
+
+    const {name, email, password} = createAccountBodySchema.parse(body)
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where:{
+        email
+      }
+    })
+
+    if(userWithSameEmail){
+      throw new ConflictException('E-mail already in use')
+    }
+
+    const hashedPassword = await hash(password,8)
+
+    await this.prisma.user.create({data:{
+      email,name,password: hashedPassword
+    }})
+
+  }
+}
