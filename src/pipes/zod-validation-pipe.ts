85047c47811400d23c5dc383e2a6d5c923d0e193
PeZunino <pedrozsantos@hotmail.com>
2025-03-07 15:41:55 -0300

    feat(validation): zod validation pipe

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+import { PipeTransform, BadRequestException, flatten } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  // unico metodo em um pipe, vai receber o valor e fazer algum transformacao.
+  transform(value: unknown) { 
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: error.flatten().fieldErrors
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+    
+    return value
+  }
+}
