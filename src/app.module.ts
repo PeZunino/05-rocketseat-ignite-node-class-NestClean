4c25f1bc324b94aad6ee6405a30460ca9e3229d5
PeZunino <pedrozsantos@hotmail.com>
2025-03-10 16:59:28 -0300

    feat(auth): create jwt auth module and auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true // global para todos os modulos
-  })],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true // global para todos os modulos
+    }),
+    AuthModule
+  ],
+  controllers: [CreateQuestionController, CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 
