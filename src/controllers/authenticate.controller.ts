fb7d67bb254f73da4489cf4f4c4d0bb758ccc06d
PeZunino <pedrozsantos@hotmail.com>
2025-03-10 21:13:49 -0300

    chore: paths

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,9 +1,8 @@
-import { Body, ConflictException, Controller, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { Prisma } from "@prisma/client";
-import { compare, hash } from "bcryptjs";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
-import { PrismaService } from "src/prisma/prisma.service";
+import { compare } from "bcryptjs";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { PrismaService } from "@/prisma/prisma.service";
 import { z } from "zod";
 
 const authenticateBodySchema = z.object({
