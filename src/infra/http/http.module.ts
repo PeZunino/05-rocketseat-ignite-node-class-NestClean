7ad740e2910c2ae37615f72a1c26485b91e35c4f
PeZunino <pedrozsantos@hotmail.com>
2025-03-11 16:46:06 -0300

    feat(infra): create HttpModule for Controllers

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
+
+@Module({
+	controllers: [
+		CreateQuestionController, 
+		CreateAccountController, 
+		AuthenticateController, 
+		FetchRecentQuestionsController
+	],
+	providers: [
+		PrismaService
+	],
+})
+export class HttpModule{}
