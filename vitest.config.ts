46607dcd9ce1dbc48741ab0ffff794305b5b7016
PeZunino <pedrozsantos@hotmail.com>
2025-03-11 15:24:36 -0300

    feat(test): exclude e2e tests from Vitest execution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,19 @@
-import swc from 'unplugin-swc'
-import { defineConfig } from 'vitest/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    root: './',
-  },
-  plugins: [
-    tsConfigPaths(),
-    swc.vite({
-      module: { type: 'es6' },
-    }),
-  ],
-})
+	test: {
+		exclude: [
+			'**/*.e2e.spec.ts',
+			'node_modules', 
+			'dist' 
+		],
+		globals: true,
+		root: './',
+	},
+	plugins: [
+		tsConfigPaths(),
+		swc.vite({module: { type: 'es6' },}),
+	],
+});
