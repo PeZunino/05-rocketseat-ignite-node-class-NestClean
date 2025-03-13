ad3b75dd1f98b5ed480f088d320283c610eee645
PeZunino <pedrozsantos@hotmail.com>
2025-03-13 19:12:52 -0300

    feat(question): implement repositories

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,20 @@
+import { Question as PrismaQuestion} from '@prisma/client';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Question } from '@/domain/forum/enterprise/entities/question';
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
+ 
+export class PrismaQuestionMapper{
+	static toDomain(raw:PrismaQuestion):Question{
+		// cria uma representacao de uma question existente no banco de dados
+		return Question.create({
+			title: raw.title,
+			content: raw.content,
+			authorId: new UniqueEntityID(raw.authorId),
+			bestAnswerId: raw.bestAnswerId ? new UniqueEntityID(raw.bestAnswerId) : null,
+			slug: Slug.create(raw.slug),
+			createdAt: raw.createdAt,
+			updatedAt: raw.updatedAt
+
+		}, new UniqueEntityID(raw.id));
+	}
+}
