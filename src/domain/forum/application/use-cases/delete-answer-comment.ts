9040634e6fa0144fd75c050172c15660e03409c9
PeZunino <pedrozsantos@hotmail.com>
2025-04-04 15:06:03 -0300

    feat(answer): delete answer comment route

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common';
 import { Either, left, right } from '@/core/either';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error';
@@ -13,6 +14,7 @@ type DeleteAnswerCommentUseCaseResponse = Either<
 	object
 >
 
+@Injectable()
 export class DeleteAnswerCommentUseCase {
 	constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
